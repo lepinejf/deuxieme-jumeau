faa57ee39a9840608cba759b8eec98d21be01f5a
Jean-Francois Lepine <lepinejf@gmail.comm>
2017-09-29 14:48:39 -0400

    wip homepage

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="stylus">
+  @import 'jeet'
+  @import 'rupture'
+  @import 'kouto-swiss'
+
+  @import url('https://fonts.googleapis.com/css?family=Titillium+Web:300,400,700')
+
+  meyer-reset()
+  normalize()
+
+  body
+    background-color #f5f5f5
+    color #343332
+    font-family 'Titillium Web', sans-serif
+
+  #app
+    display block
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="intro">
+    <div>
+      <p>
+        Je <b>crée</b><br/>
+        des <b>sites web</b>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  .intro
+    size 100vh
+
+    div
+      align(c)
+      font-size 60px
+      text-align center
+
+      +tablet()
+        font-size 40px
+
+      +mobile()
+        font-size 28px
+</style>
